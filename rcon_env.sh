b1dc0303dc56192018743765f6dc9e03b1aeca08
Jacob Ludriks <jacob@ludriks.com>
2021-05-26 20:06:31 +1000

    adding key vault

---
+++ rcon_env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# The -v flag is for the Azure Key Vault name. Assign to variable $vault
+while getopts v: flag
+do
+    case "${flag}" in
+        v) vault=${OPTARG};;
+    esac
+done
+
+# Retrieve access token using system managed identity
+accesstoken=$(curl -s 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net' -H Metadata:true | jq --raw-output .access_token)
+
+# Access Azure Key Vault using access token
+rconpassword=$(curl -s 'https://tf2server.vault.azure.net/secrets/rconpassword?api-version=2016-10-01' -H "Authorization:Bearer $access_token" | jq --raw-output .value)
+
+# Export to an environmental variable
+export RCON_PWD=$rconpassword
